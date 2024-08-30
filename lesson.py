ffbaa8d65b344165a2ddca241530e10b4509579d
Hernandez <dhernandez209@cps.k12.il.us>
2024-08-30 15:06:30 -0500

    first commit

---
+++ lesson.py
@@ -0,0 +1,61 @@
+#create a print function that prints
+# out the string "Hello, World
+# print("Hello, World")
+# ask the user for the day of the weeks
+# day_of_week = input("What day of the week is it? ")
+# print("today is"+day_of_week)
+# concatenacion is when you add two strings together
+# using a plus sign (+)
+# Movies_this_week = input("what movie are you watching this week? ")
+# print("I am watching" + Movies_this_week + "this week")
+# mood= input("How are you feeling today? ")
+# print("I am feeling" + mood )
+
+#data types for variables in python
+# strings - text
+name = "John" # rhis is a string data type
+#whenever it is wrapped in quotes it is
+year = "2024"
+# integers - whole numbers
+year = 2024 # this is an integer data type 
+# does not need to be wrapped in quotes
+yearfourfromnow = 2028
+subtract = yearfourfromnow - year
+print(subtract)
+#floats - decimal numbers
+pricebigmac= 3.99 # this is a float data type
+pricedoublepounder= 4.99
+totalprice = pricebigmac + pricedoublepounder
+print(totalprice)
+# needs to have a decimal point
+# booleans - true or false 
+isRaining= false # this is a boolean data type 
+print(isRaining)
+# lists - a collection of items
+groceries= ("apples", "bananas", "carrots")
+print(groceries)
+#challenge #1
+#you and your family are going to a movie and 
+# dinner. you need a list of movies to choose from
+listofmovie=("The matrix", "deadpool" )
+# the address of the restaurant
+restaurantaddress= "123 main dr"
+
+restaurantname= "dennys"
+#the time of the movie
+timeofmovie= "4:30"
+#the time of the dinner
+timeofdinner= "3:00"
+# the name of the movie
+moviename= "the matrix"
+# the price  of the movie
+movieprice= 15.99
+# the price of the of the dinner
+dinnerprice= 30.49
+# check to see if the movie is playing in the evening
+Inevening= True
+#how many people are going
+peoplegoing= input("How many people are going? ")
+#print the entire output in one sentence
+# print the entire output in one sentence
+print("we are going to see"+ moviename + )
